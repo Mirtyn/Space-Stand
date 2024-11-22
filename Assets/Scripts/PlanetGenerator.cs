575cedccc0f413c65851cede515b14e6a7c673f1
epmik <steven.kimpe@tothedot.be>
2024-11-22 23:06:43 +0100

    development

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -35,12 +35,78 @@ public static class PlanetGenerator
 
         var meshFilter = planetGameObject.GetComponent<MeshFilter>();
 
-        var vertices = new Vector3[]
+        const float width = 1f;
+        const float height = 1f;
+        const int widthSegments = 4;
+        const int heightSegments = 4;
+
+        const float width_half = width / 2;
+        const float height_half = height / 2;
+
+        const int gridX = widthSegments;
+        const int gridY = heightSegments;
+
+        const int gridX1 = gridX + 1;
+        const int gridY1 = gridY + 1;
+
+        const float segment_width = width / (float)gridX;
+        const float segment_height = height / (float)gridY;
+
+        var vertices = new List<Vector3>();
+        var normals = new List<Vector3>();
+        var uvs = new List<Vector2>();
+        var triangles = new List<int>();
+
+        for (var iy = 0; iy < gridY1; iy++)
         {
-            new Vector3(0.0f, 0.0f, 0.0f),
-            new Vector3(0.0f, 1.0f, 0.0f),
-            new Vector3(1.0f, 0.0f, 0.0f),
-        };
+            float y = iy * segment_height - height_half;
+
+            for (var ix = 0; ix < gridX1; ix++)
+            {
+                float x = ix * segment_width - width_half;
+
+                float xx = x + width_half;
+
+                float yy = y + height_half;
+
+                float xCircle = xx + width_half * Mathf.Sqrt(1f - 0.5f * yy * yy);
+                float yCircle = yy * Mathf.Sqrt(1f - 0.5f * xx * xx);
+
+                //vertices.Add(new Vector3(x, -y, 0));
+                vertices.Add(new Vector3(xCircle, -yCircle, 0));
+
+                normals.Add(new Vector3(0, 0, -1));
+
+                uvs.Add(new Vector2(ix / gridX, 1f - (iy / gridY)));
+            }
+        }
+
+        for (var iy = 0; iy < gridY; iy++)
+        {
+            for (var ix = 0; ix < gridX; ix++)
+            {
+                int a = ix + gridX1 * iy;
+                int b = ix + gridX1 * (iy + 1);
+                int c = (ix + 1) + gridX1 * (iy + 1);
+                int d = (ix + 1) + gridX1 * iy;
+
+                triangles.Add(a);
+                triangles.Add(d);
+                triangles.Add(b);
+
+                triangles.Add(b);
+                triangles.Add(d);
+                triangles.Add(c);
+            }
+        }
+
+
+        //var vertices = new Vector3[]
+        //{
+        //    new Vector3(0.0f, 0.0f, 0.0f),
+        //    new Vector3(0.0f, 1.0f, 0.0f),
+        //    new Vector3(1.0f, 0.0f, 0.0f),
+        //};
 
         //var colors = new Color32[]
         //{
@@ -49,21 +115,23 @@ public static class PlanetGenerator
         //    new Color32(0, 255, 255, 255),
         //};
 
-        var triangles = new int[]
-        {
-            0, 1, 2,
-        };
+        //var triangles = new int[]
+        //{
+        //    0, 1, 2,
+        //};
 
         var mesh = meshFilter.mesh;
 
         mesh.Clear();
 
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.vertices = vertices.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = triangles.ToArray();
         //mesh.colors32 = colors;
 
         //mesh.Optimize();
-        mesh.RecalculateNormals();
+        //mesh.RecalculateNormals();
 
         planetGameObject.name = "Planet";
         planetGameObject.transform.SetParent(_rootGameObject.transform, false);
